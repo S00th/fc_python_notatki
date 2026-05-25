acce3212791d0ce0568acdf5d933111a8cb4d10c
SzymonB <5007h@tutamail.com>
2026-05-25 11:07:55 +0200

    EDIT: 00_draft

--- 00_draft.py
+++ 00_draft.py
@@ -1,19 +1,2 @@
-text = input('Wpisz dowolny tekst: ')
-center = len(text) // 2
-modulo = len(text) % 2
-correct_text = True
+####### DRAFT
 
-if text[0] != '@':
-    print('Tekst MUSI zaczynać się od @.')
-    correct_text = False
-if text[-1].isalpha():
-    print('Tekst MUSI kończyć się liczbą.')
-    correct_text = False
-if text[center] != 'x':
-    print('Środkowym znakiem tekstu MUSI być X.')
-    correct_text = False
-if text[modulo] != 'x':
-    print('Środkowym znakiem tekstu MUSI być X.')
-    correct_text = False
-if correct_text:
-    print('Wiadomość zaakceptowana.')
