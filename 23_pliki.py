e82baa8acf4007e3ec62955728f913bcec314f5d
SzymonB <5007h@tutamail.com>
2026-06-30 20:15:38 +0200

    Edit

--- 23_pliki.py
+++ 23_pliki.py
@@ -371,3 +371,32 @@ with open('plik_csv/new_scv.scv', mode='w', encoding='utf-8', newline='') as fil
 # 1. Zaczytuję dane z pliku osoby.csv
 # 2. Zapisuje do pliku pełnoletni.csv tylko te wpisy, gdzie osoba ma powyżej 18 lat
 # 3.
+
+
+
+import csv
+
+with open('data/osoby.csv', encoding='utf-8') as infile:
+    # reader = csv.DictReader(infile,)
+    # print(list(reader))
+
+    adults = []
+    for osoba in reader:
+        if int(osoba['wiek']) >= 18:
+            print(osoba)
+            adults.append(osoba)
+        print(adults)
+
+with open('data/adults.csv', mode='w', encoding='utf-8', newline='') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames=['imie', 'nazwisko', 'wiek'])
+    writer.writeheader()
+    writer.writerows(adults)
+
+# Zapis w jednej linijce
+
+    adults = [osoba for osoba in reader if int(osoba['wiek']) >= 18]
+
+with open('data/adults.csv', mode='w', encoding='utf-8', newline='') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames=['imie', 'nazwisko', 'wiek'])
+    writer.writeheader()
+    writer.writerows(adults)
